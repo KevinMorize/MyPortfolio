ae625fe8f7dd4eb59d19206407f00777a52babfd
KevinMorize <webdevbox91@gmail.com>
2021-10-26 15:50:21 +0200

    work on language 3rd

--- src/data/mainContentData.js
+++ src/data/mainContentData.js
@@ -1,32 +1,32 @@
 export const mainContentData = [
+  {
+      id: 1,
+      title: ["KEVIN", "KEVIN"],
+      title2: ["MORIZE", "MORIZE"],
+      content: ["développeur web junior", "junior web developer"]
+    },
     {
-        id: 1,
-        title: "KEVIN",
-        title2: "MORIZE",
-        content: "junior web developer"
-      },
-      {
-        id: 2,
-        title: "About Me",
-        content: "I love gaming, cooking and coding",
-        img: "./assets/img/about.png",
-        number: "01",
-        link: "/about/content",
-      },
-      {
-        id: 3,
-        title: "My Work",
-        content: "Some projects I like",
-        img: "./assets/img/work.png",
-        number: "02",
-        link: "/production/content",
-      },
-      {
-        id: 4,
-        title: "Get In Touch",
-        email: "morizekevin91@gmail.com",
-        phone: "07 81 93 81 54",
-        img: "./assets/img/contact.png",
-        number: "03",
-      },
+      id: 2,
+      title: ["À propos", "About Me"],
+      content: ["J'aime jouer, coder et cuisiner.", "I love gaming, cooking and coding."],
+      img: "./assets/img/about.png",
+      number: "01",
+      link: "/about/content",
+    },
+    {
+      id: 3,
+      title: ["Mon travail","My Work"],
+      content: ["Mes projets passés et à venir", "Some projects I'd like to show"],
+      img: "./assets/img/work.png",
+      number: "02",
+      link: "/production/content",
+    },
+    {
+      id: 4,
+      title: ["Contact","Get In Touch"],
+      email: "morizekevin91@gmail.com",
+      phone: "07 81 93 81 54",
+      img: "./assets/img/contact.png",
+      number: "03",
+    },
 ];
